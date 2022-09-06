42afe0c8f2746879fc632932efc52addbec93d0e
Olha Cheroi <107466851+olya4e@users.noreply.github.com>
2022-09-06 15:22:26 +0300

    add npm simpleligthbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryBoxEl = document.querySelector('.gallery');
+galleryBoxEl.innerHTML = galleryItems
+  .map(el => {
+    return `<div class="gallery__item">
+  <a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>
+</div>`;
+  })
+  .join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
