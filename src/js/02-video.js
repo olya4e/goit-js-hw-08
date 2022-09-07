b6925e2d227ca39055ef18c19d444e12874d211c
Olha Cheroi <107466851+olya4e@users.noreply.github.com>
2022-09-07 15:26:34 +0300

    add task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
 
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import localstorageService from './localstorage';
+const throttle = require('lodash.throttle');
+const contactFormEl = document.querySelector('.feedback-form');
+const userData = {};
 
+const onFormFieldInput = event => {
+  const { target } = event;
+  const fieldName = target.name;
+  const fieldValue = target.value;
+  userData[fieldName] = fieldValue;
+  localstorageService.save('feedback-form-state', userData);
+};
+
+const onFillContactForm = () => {
+  const contactFormDataLS = localstorageService.load('feedback-form-state');
+  for (const item in contactFormDataLS) {
+    if (contactFormDataLS.hasOwnProperty(item)) {
+      contactFormEl.elements[item].value = contactFormDataLS[item];
+    }
+  }
+};
+onFillContactForm();
+
+const onContactFormSubmit = event => {
+  const {
+    elements: { email, message },
+  } = event.target;
+  event.preventDefault();
+  if (email.value.length === 0 || message.value.length === 0) {
+    return;
+  }
+  localstorageService.remove('feedback-form-state');
+  contactFormEl.reset();
+  console.log(userData);
+};
+
+contactFormEl.addEventListener('input', throttle(onFormFieldInput, 500));
+contactFormEl.addEventListener('submit', onContactFormSubmit);
